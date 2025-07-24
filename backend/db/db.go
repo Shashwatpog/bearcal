80d34830063e48618c457318c050ed3432f85033
Shashwat <mishras8@mail.uc.edu>
2025-07-24 03:43:06 -0400

    added env loading and db connection

---
+++ backend/db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func Connnect() {
+	var err error
+	DB, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Error connecting to DB", err)
+	}
+	if err = DB.Ping(); err != nil {
+		log.Fatal("Error pinging DB", err)
+	}
+	log.Println("Connected to supabase DB")
+}
--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/Shashwatpog/bearcal/backend/db"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("init commit lol")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env file not found")
+
+	}
+
+	db.Connnect()
 }
